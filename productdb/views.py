7b6181e64f7918b979701012001eff42e9798e91
Sunil-Kumar-P <sunilkpg23@gmail.com>
2021-10-06 13:15:07 +0530

    06/11/2021

--- productdb/views.py
+++ productdb/views.py
@@ -6,26 +6,26 @@ from tablib import Dataset
 from django.http import HttpResponse
 
 # Create your views here.
-def dataUpload(request):
-    if request.method == 'POST':
-        product_resource =productResource()
-        dataset = Dataset()
-        new_product = request.FILES['myfile']
+# def dataUpload(request):
+#     if request.method == 'POST':
+#         product_resource =productResource()
+#         dataset = Dataset()
+#         new_product = request.FILES['myfile']
 
-        if not new_product.name.endswith('xlsx'):
-            messages.info(request,'wrong format')
-            return render(request,'upload.html')
+#         if not new_product.name.endswith('xlsx'):
+#             messages.info(request,'wrong format')
+#             return render(request,'upload.html')
 
-        imported_data = dataset.load(new_product.read(),format='xlsx')
-        for data in imported_data:
-            value = product_details(
-                data[0],
-                data[1],
-                data[2],
-                data[3]
-                )
-            value.save()
-    return render(request,'upload.html')
+#         imported_data = dataset.load(new_product.read(),format='xlsx')
+#         for data in imported_data:
+#             value = product_details(
+#                 data[0],
+#                 data[1],
+#                 data[2],
+#                 data[3]
+#                 )
+#             value.save()
+#     return render(request,'upload.html')
 
 
 def product_list(request):
